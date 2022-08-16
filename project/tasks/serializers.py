4ff0444915b050227e91df2d2a1bcd6d065de7b7
Arya <aryaajitnair@gmail.com>
2022-08-17 00:10:24 +0530

    pagination, filtering and ordering setup

--- project/tasks/serializers.py
+++ project/tasks/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from tasks.models import Category
+from tasks.models import Category, Task
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
+        fields='__all__'
+        read_only_fields=['created_by']
+
+class TaskSerializer(serializers.ModelSerializer):
+    category_name=serializers.CharField(source='category.name',read_only=True)
+    category_color=serializers.CharField(source='category.color',read_only=True)
+
+    class Meta:
+        model=Task
         fields='__all__'
         read_only_fields=['created_by']
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .serializers import CategorySerializer
-from .models import Category
-from rest_framework import viewsets, permissions
+from .serializers import CategorySerializer, TaskSerializer
+from .models import Category, Task
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+
 
 # Create your views here.
+class StandardResultSetPagination(PageNumberPagination):
+    page_size=6
+    page_size_query_param='page_size'
+    max_page_size=6 
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -12,5 +18,38 @@ class CategoryViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.request.user.categories.all()
     
+    def perform_create(self,serializer):
+        serializer.save(created_by=self.request.user)
+
+class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes=[permissions.IsAuthenticated]
+    serializer_class=TaskSerializer
+    pagination_class=StandardResultSetPagination
+    filter_backends=[
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    search_fields=['title']
+    ordering_fields=['completed']
+    ordering=['completed']
+
+    def get_queryset(self):
+        user=self.request.user
+        completed=self.request.query_params.get('completed')
+        priority=self.request.query_params.get('priority')
+        category=self.request.query_params.get('category')
+        query_params={}
+
+        if completed is not None:
+            query_params['completed']=completed
+
+        if priority is not None:
+            query_params['priority']=priority
+        
+        if category is not None:
+            query_params['category']=category
+
+
+        return Task.objects.filter(created_by=user,**query_params)
     def perform_create(self,serializer):
         serializer.save(created_by=self.request.user)
